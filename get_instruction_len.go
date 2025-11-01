3820494dcc5691c7a98b982cd1a7bf14edc3c9c0
Connor van Spronssen <info@dobefu.com>
2025-11-01 13:41:17 +0100

    refactor: Use a map instead of a switch statement for instruction lengths

--- get_instruction_len.go
+++ get_instruction_len.go
@@ -1,135 +1,57 @@
 package vm
 
+// Note: The instruction length includes the opcode itself.
+var instructionLengths = map[Opcode]register{
+	OpcodeNop:                          1,
+	OpcodePush:                         2,
+	OpcodePop:                          2,
+	OpcodeLoadImmediate:                10,
+	OpcodeLoadRegister:                 3,
+	OpcodeLoadMemory:                   3,
+	OpcodeStoreMemory:                  3,
+	OpcodeAdd:                          4,
+	OpcodeSub:                          4,
+	OpcodeMul:                          4,
+	OpcodeDiv:                          4,
+	OpcodeMod:                          4,
+	OpcodeAND:                          4,
+	OpcodeOR:                           4,
+	OpcodeXOR:                          4,
+	OpcodeNOT:                          3,
+	OpcodeShiftLeft:                    4,
+	OpcodeShiftRight:                   4,
+	OpcodeShiftRightArithmetic:         4,
+	OpcodeCMP:                          3,
+	OpcodeJmpImmediate:                 9,
+	OpcodeJmpImmediateIfZero:           10,
+	OpcodeJmpImmediateIfNotZero:        10,
+	OpcodeJmpImmediateIfEqual:          9,
+	OpcodeJmpImmediateIfNotEqual:       9,
+	OpcodeJmpImmediateIfGreater:        9,
+	OpcodeJmpImmediateIfGreaterOrEqual: 9,
+	OpcodeJmpImmediateIfLess:           9,
+	OpcodeJmpImmediateIfLessOrEqual:    9,
+	OpcodeJmpRegister:                  2,
+	OpcodeJmpRegisterIfZero:            3,
+	OpcodeJmpRegisterIfNotZero:         3,
+	OpcodeJmpRegisterIfEqual:           2,
+	OpcodeJmpRegisterIfNotEqual:        2,
+	OpcodeJmpRegisterIfGreater:         2,
+	OpcodeJmpRegisterIfGreaterOrEqual:  2,
+	OpcodeJmpRegisterIfLess:            2,
+	OpcodeJmpRegisterIfLessOrEqual:     2,
+	OpcodeCallImmediate:                9,
+	OpcodeCallRegister:                 2,
+	OpcodeReturn:                       1,
+	OpcodeHalt:                         1,
+}
+
 func (v *VM) getInstructionLen(opcode Opcode) register {
-	// Note: The instruction length includes the opcode itself.
-	switch opcode {
-	case OpcodeNop:
-		return 1
+	length, ok := instructionLengths[opcode]
 
-	case OpcodePush:
-		return 2
-
-	case OpcodePop:
-		return 2
-
-	case OpcodeLoadImmediate:
-		return 10
-
-	case OpcodeLoadRegister:
-		return 3
-
-	case OpcodeLoadMemory:
-		return 3
-
-	case OpcodeStoreMemory:
-		return 3
-
-	case OpcodeAdd:
-		return 4
-
-	case OpcodeSub:
-		return 4
-
-	case OpcodeMul:
-		return 4
-
-	case OpcodeDiv:
-		return 4
-
-	case OpcodeMod:
-		return 4
-
-	case OpcodeAND:
-		return 4
-
-	case OpcodeOR:
-		return 4
-
-	case OpcodeXOR:
-		return 4
-
-	case OpcodeNOT:
-		return 3
-
-	case OpcodeShiftLeft:
-		return 4
-
-	case OpcodeShiftRight:
-		return 4
-
-	case OpcodeShiftRightArithmetic:
-		return 4
-
-	case OpcodeCMP:
-		return 3
-
-	case OpcodeJmpImmediate:
-		return 9
-
-	case OpcodeJmpImmediateIfZero:
-		return 10
-
-	case OpcodeJmpImmediateIfNotZero:
-		return 10
-
-	case OpcodeJmpImmediateIfEqual:
-		return 9
-
-	case OpcodeJmpImmediateIfNotEqual:
-		return 9
-
-	case OpcodeJmpImmediateIfGreater:
-		return 9
-
-	case OpcodeJmpImmediateIfGreaterOrEqual:
-		return 9
-
-	case OpcodeJmpImmediateIfLess:
-		return 9
-
-	case OpcodeJmpImmediateIfLessOrEqual:
-		return 9
-
-	case OpcodeJmpRegister:
-		return 2
-
-	case OpcodeJmpRegisterIfZero:
-		return 3
-
-	case OpcodeJmpRegisterIfNotZero:
-		return 3
-
-	case OpcodeJmpRegisterIfEqual:
-		return 2
-
-	case OpcodeJmpRegisterIfNotEqual:
-		return 2
-
-	case OpcodeJmpRegisterIfGreater:
-		return 2
-
-	case OpcodeJmpRegisterIfGreaterOrEqual:
-		return 2
-
-	case OpcodeJmpRegisterIfLess:
-		return 2
-
-	case OpcodeJmpRegisterIfLessOrEqual:
-		return 2
-
-	case OpcodeCallImmediate:
-		return 9
-
-	case OpcodeCallRegister:
-		return 2
-
-	case OpcodeReturn:
-		return 1
-
-	case OpcodeHalt:
-		return 1
-
-	default:
+	if !ok {
 		return 0
 	}
+
+	return length
 }
