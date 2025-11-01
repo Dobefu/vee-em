171aa3ea0cb5046c72576832e5ee2e623739f6be
Connor van Spronssen <info@dobefu.com>
2025-11-01 11:52:10 +0100

    test: Add tests for the jump register if greater than opcode

--- run_test.go
+++ run_test.go
@@ -658,6 +658,44 @@ func TestRun(t *testing.T) {
 			},
 		},
 		{
+			name: "jmp register if greater",
+			program: []byte{
+				byte(OpcodeLoadImmediate), 0, 0, 0, 0, 0, 0, 0, 0, 2,
+				byte(OpcodeLoadImmediate), 1, 0, 0, 0, 0, 0, 0, 0, 1,
+				byte(OpcodeLoadImmediate), 2, 0, 0, 0, 0, 0, 0, 0, 45,
+				byte(OpcodeCMP), 0, 1,
+				byte(OpcodeJmpRegisterIfGreater), 2,
+				byte(OpcodeLoadImmediate), 3, 0, 0, 0, 0, 0, 0, 0, 2, // This should get skipped.
+				byte(OpcodeLoadImmediate), 0, 0, 0, 0, 0, 0, 0, 0, 2,
+			},
+			expectedRegisters: [NumRegisters]int64{2, 1, 45},
+			expectedFlags: flags{
+				isZero:      false,
+				isNegative:  false,
+				hasCarry:    false,
+				hasOverflow: false,
+			},
+		},
+		{
+			name: "jmp register if greater (not greater)",
+			program: []byte{
+				byte(OpcodeLoadImmediate), 0, 0, 0, 0, 0, 0, 0, 0, 1,
+				byte(OpcodeLoadImmediate), 1, 0, 0, 0, 0, 0, 0, 0, 1,
+				byte(OpcodeLoadImmediate), 2, 0, 0, 0, 0, 0, 0, 0, 45,
+				byte(OpcodeCMP), 0, 1,
+				byte(OpcodeJmpRegisterIfGreater), 2,
+				byte(OpcodeLoadImmediate), 3, 0, 0, 0, 0, 0, 0, 0, 2, // This should not get skipped.
+				byte(OpcodeLoadImmediate), 0, 0, 0, 0, 0, 0, 0, 0, 2,
+			},
+			expectedRegisters: [NumRegisters]int64{2, 1, 45, 2},
+			expectedFlags: flags{
+				isZero:      true,
+				isNegative:  false,
+				hasCarry:    false,
+				hasOverflow: false,
+			},
+		},
+		{
 			name: "cmp",
 			program: []byte{
 				byte(OpcodeLoadImmediate), 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -942,6 +980,14 @@ func TestRunErr(t *testing.T) {
 			expected: errors.New("unexpected end of program"),
 		},
 		{
+			name: "opcode jmp register if greater too few arguments",
+			program: []byte{
+				0x00,
+				byte(OpcodeJmpRegisterIfGreater),
+			},
+			expected: errors.New("unexpected end of program"),
+		},
+		{
 			name: "division by zero",
 			program: []byte{
 				0x00,
@@ -1121,6 +1167,18 @@ func TestRunErr(t *testing.T) {
 				byte(OpcodeCMP), 0, 1,
 				byte(OpcodeLoadImmediate), 2, 0, 0, 0, 0, 0, 0, 0, 36,
 				byte(OpcodeJmpRegisterIfNotEqual), 2,
+			},
+			expected: errors.New("memory address out of bounds"),
+		},
+		{
+			name: "jmp register if greater memory address out of bounds",
+			program: []byte{
+				0x00,
+				byte(OpcodeLoadImmediate), 0, 0, 0, 0, 0, 0, 0, 0, 2,
+				byte(OpcodeLoadImmediate), 1, 0, 0, 0, 0, 0, 0, 0, 1,
+				byte(OpcodeCMP), 0, 1,
+				byte(OpcodeLoadImmediate), 2, 0, 0, 0, 0, 0, 0, 0, 36,
+				byte(OpcodeJmpRegisterIfGreater), 2,
 			},
 			expected: errors.New("memory address out of bounds"),
 		},
